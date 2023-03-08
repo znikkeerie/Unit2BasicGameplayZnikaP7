9aa7118c6bc7ab0087e1198612c1b663f373e18b
znikkeerie <keiraznika@gmail.com>
2023-03-08 15:14:50 -0800

    Progress March 8th 2022
    Finished another section

--- Unit2BasicGameplayZnikaP7/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGameplayZnikaP7/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange =15.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +30,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
