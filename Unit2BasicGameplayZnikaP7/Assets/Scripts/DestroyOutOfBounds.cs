66ae274cf3ec581f7e4224432901a904e4e82127
znikkeerie <keiraznika@gmail.com>
2023-03-14 15:25:25 -0700

    Finished game
    Now need bonus feature add ons

--- Unit2BasicGameplayZnikaP7/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2BasicGameplayZnikaP7/Assets/Scripts/DestroyOutOfBounds.cs
@@ -9,17 +9,21 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()  
+    void Update()
     {
+
+        // If an object goes past the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
